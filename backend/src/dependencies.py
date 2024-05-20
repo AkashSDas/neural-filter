615e728ac7fc6ccc1648ab7ffed8f6b51844f117
AkashSDas <aakashdas368@gmail.com>
2024-05-20 21:57:42 +0530

    [be] add me route and handler

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,7 +1,18 @@
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from pydantic import BaseModel
+from src.models.user import User
+from src.utils.settings import get_settings
 from src.utils.database import SessionLocal
 from sqlalchemy.orm import Session
+
+settings = get_settings()
+
+# ============================
+# Database
+# ============================
 
 
 async def get_db():
@@ -13,3 +24,47 @@ async def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+# ============================
+# Auth
+# ============================
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+class TokenData(BaseModel):
+    email: str | None
+
+
+class AuthUser(BaseModel):
+    email: str | None
+
+
+async def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)], db: db_dependency
+) -> dict:
+    cred_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials"
+    )
+
+    try:
+        payload = jwt.decode(
+            token, settings.jwt_secret_key, algorithms=[settings.jwt_algorithm]
+        )
+        email = payload.get("sub")
+        if not isinstance(email, str):
+            raise cred_exception
+        else:
+            token_data = TokenData(email=email)
+    except JWTError:
+        raise cred_exception
+
+    user = db.query(User).filter(User.email == token_data.email).first()
+    if not user:
+        raise cred_exception
+
+    return AuthUser(email=user.email).model_dump()  # type: ignore
+
+
+current_user = Annotated[dict, Depends(get_current_user)]
--- backend/src/routers/auth.py
+++ backend/src/routers/auth.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 from fastapi import APIRouter, Body, HTTPException, status
-from src.schemas.auth import SignupIn, UserInDB, LoginIn, LoginOut, LogoutOut
-from src.dependencies import db_dependency
+from src.schemas.auth import SignupIn, UserInDB, LoginIn, LoginOut, LogoutOut, SignupOut
+from src.dependencies import db_dependency, current_user
 from src.models.user import User
 from src.utils.auth import create_access_token
 
@@ -25,7 +25,16 @@ async def signup(body: Annotated[SignupIn, Body()], db: db_dependency) -> dict:
     db.refresh(new_user)
 
     access_token = create_access_token(data={"sub": new_user.email})
-    return {"user": new_user, "access_token": access_token}
+    user = UserInDB(id=new_user.id, username=new_user.username, email=new_user.email).model_dump()  # type: ignore
+    return SignupOut(user=user, access_token=access_token).model_dump()  # type: ignore
+
+
+@router.get("/me", status_code=status.HTTP_200_OK)
+async def me(user: current_user, db: db_dependency) -> dict:
+    current_user = db.query(User).filter(User.email == user["email"]).first()
+    return {
+        "user": UserInDB(id=current_user.id, username=current_user.username, email=current_user.email).model_dump()  # type: ignore
+    }
 
 
 @router.post("/login", status_code=status.HTTP_200_OK, response_model=LoginOut)
